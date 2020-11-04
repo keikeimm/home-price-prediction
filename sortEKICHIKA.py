279cd645622d780da31ac526387c86fcb12ccf8d
keisuke <kei11suke19@icloud.com>
2020-11-05 00:12:27 +0900

    second commit2

--- sortEKICHIKA.py
+++ sortEKICHIKA.py
@@ -1,6 +1,7 @@
 # 必要なライブラリをインポート
 import pandas as pd
 import numpy as np
+#numpyは必要ないと思いましたが念のため入れときました。
 
 df_oookayama = pd.read_csv('suumo_oookayama_data.csv', sep='\t', encoding='utf-16')
 
@@ -12,11 +13,10 @@ df.drop(['Unnamed: 0'], axis=1, inplace=True)
 for x in range(len(df)):
     if not df['徒歩2'][x] is None:
         if df['徒歩1'][x] > df['徒歩2'][x]:
-            df['徒歩1'][x] , df['徒歩2'][x] = df['徒歩2'][x] , df['徒歩1'][x]
+            df['徒歩1'][x], df['路線1'][x], df['駅1'][x], df['徒歩2'][x], df['路線2'][x], df['駅2'][x] = df['徒歩2'][x], df['徒歩2'][x], df['駅2'][x], df['徒歩1'][x], df['路線1'][x], df['駅1'][x]
         
     if not df['徒歩3'][x] is None:
         if df['徒歩1'][x] > df['徒歩3'][x]:
-            df['徒歩1'][x] , df['徒歩3'][x] = df['徒歩3'][x] , df['徒歩1'][x]
+            df['徒歩1'][x], df['路線1'][x], df['駅1'][x], df['徒歩3'][x], df['路線3'][x], df['駅3'][x] = df['徒歩3'][x], df['徒歩3'][x], df['駅3'][x], df['徒歩1'][x], df['路線1'][x], df['駅1'][x]
         
-
 df.to_csv('suumo_oookayama_data2.csv', sep='\t', encoding='utf-16')
