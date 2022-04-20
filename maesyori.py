d41466194eca51302600428ca449a25ebb59f091
keikeimm <72242870+keikeimm@users.noreply.github.com>
2022-04-20 16:41:14 +0900

    Update maesyori.py

--- maesyori.py
+++ maesyori.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-df_oookayama = pd.read_csv('suumo_oookayama_around.csv', sep='\t', encoding='utf-16')
+df_oookayama = pd.read_csv('example.csv', sep='\t', encoding='utf-16')
 # 敷金と礼金は一つ一つのデータ
 
 # axis=0は縦方向に連結、ignore_indes=Trueは初めに与えられたindexを無視する。
